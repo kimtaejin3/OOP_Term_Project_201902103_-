9d9d8fba5bac6ef1bff8c96c53a11fc1a4ee13e5
kimtaejin3 <rlaxowls1316@naver.com>
2020-11-25 20:43:00 +0900

    Update view

--- OOP_Term_Project_201902103_김태진/text_edit.cpp
+++ OOP_Term_Project_201902103_김태진/text_edit.cpp
@@ -12,16 +12,39 @@ string savingSentencesWithString;
 int page = 1;
 int lastPage;
 bool isChanged = false;
+string result = "";
 
-//n에 따른 매개변수가 필요.
-void showInitTextView(int n) {
+//라인이 20개 이내일 때와 20개 이상일 때를 구분
+void showInitTextView20line() {
 	int size = savingSentences.size();
+	for (int i = 1; i <= 20; i++) {
+		if (i < 10) {
+			cout << " " << i << "| ";
+		}
+		else {
+			cout << i << "| ";
+		}
+		if (size > i) {
+			cout << savingSentences[i - 1];
+		}
+		cout << "\n";
+	}
+}
+
+void showInitTextView(int n) {
+
+	int size = savingSentences.size();
+	if (size < 20) {
+		showInitTextView20line();
+		return;
+	}
 	int vectorSizeOfFirstLine = (n - 1) * 20 + 1;
 	int lackOfLine = -1;
-	for (int i = (n-1)*20+1; i < n*20 + 1; i++) {
-		
-		if (vectorSizeOfFirstLine+20 > size) {
-			lackOfLine = (20 + vectorSizeOfFirstLine)-size;
+	for (int i = (n - 1) * 20 + 1; i < n * 20 + 1; i++) {
+
+		if (vectorSizeOfFirstLine + 20 > size) {
+
+			lackOfLine = (20 + vectorSizeOfFirstLine) - size - 1;
 			cout << i - lackOfLine << "| ";
 		}
 		else
@@ -29,7 +52,7 @@ void showInitTextView(int n) {
 			if (i < 10) {
 				cout << " " << i << "| ";
 			}
-			else{
+			else {
 				cout << i << "| ";
 			}
 		}
@@ -39,11 +62,10 @@ void showInitTextView(int n) {
 			cout << "\n";
 		}
 		else if (i < size) {
-			cout << savingSentences[i-1];
+			cout << savingSentences[i - 1];
 			cout << "\n";
 		}
 	}
-	
 }
 
 void saveAndExit() {
@@ -59,34 +81,22 @@ void saveAndExit() {
 
 void getInitText() {
 	ifstream fin("test.txt");
-	//int count = 0;
 	
-	// 이것도 error클래스의 멤버변수로 넣기?
 	if (fin.peek() == ifstream::traits_type::eof()) {
-		cout << "Error opening fild" << endl;
+		cout << "디렉토리에서 test.txt를 찾지 못했습니다." << endl;
 		exit(0);
 	}
-
 
 	string str="";
 
 	while (!fin.eof()) {
 		string buf;
 		fin >> buf;
-		//int bufStringSize = buf.length();
-		//count += bufStringSize;
 		str += buf+" ";
 
-		//if (count > 45) {
-			//savingSentences.push_back(str);
-			savingSentencesWithString += str;
-			str = "";
-		//	count = 0;
-		//}
+		savingSentencesWithString += str;
+		str = "";
 	} 
-
-	// 마지막 str은 따로 push 해야 함.
-	//savingSentences.push_back(str);
 
 	fin.close();
 }
@@ -120,14 +130,70 @@ public:
 };
 */
 
-bool isValid(string str) {
-	int size = str.length();
-	if (str.at(1) == '(' && str.at(size - 1) == ')') {
+class ConfirmValidation {
+public:
+	bool isValid(string str) {
+		int size = str.length();
+		if (str.at(1) == '(' && str.at(size - 1) == ')') {
+			return true;
+		}
+		result = "출력명세는 명령어(인자, ...) 입니다.";
+		return false;
+	}
+
+	bool has_only_digits(const string s) {
+		return s.find_first_not_of("0123456789") == string::npos;
+	}
+
+	bool isOver(int row, int col) {
+		int size = savingSentences.size();
+		if (page == lastPage) {
+			if (row > size) {
+				result = "현재 출력창에는 " + to_string(row) + "라인이 존재하지 않습니다.";
+				return false;
+			}
+		}
+		else if (row >page*20) {
+			result = "현재 출력창에는 " + to_string(row) + "라인이 존재하지 않습니다.";
+			return false;
+		}
+
+		int lineLength = savingSentences[row - 1].length();
+		int maxCol=0;
+		for (int i = 0; i < lineLength; i++) {
+			if (savingSentences[row - 1][i] == ' ') {
+				++maxCol;
+			}
+		}
+		if (col > maxCol) {
+			result = to_string(col)+"번째 단어가 존재하지 않습니다.";
+			return false;
+		}
 		return true;
 	}
-	return false;
-}
 
+	bool InsertInstructionIsWrong(vector<string> vec) {
+		if (vec.size() < 3) {
+			result = "인자 세 개를 제대로 입력하세요.";
+			return false;
+		}
+
+		if (!has_only_digits(vec[0])) {
+			result = "숫자(양수)만 입력해 주세요.";
+			return false;
+		}
+		if (!has_only_digits(vec[1])) {
+			result = "숫자(양수)만 입력해 주세요.";
+			return false;
+		}
+
+		if (vec[2].length() > 75) {
+			result = "당신이 입력하고 싶은 문자열의 길이가 너무 깁니다.";
+			return false;
+		}
+		return true;
+	}
+};
 
 void stringToVector() {
 	string str;
@@ -164,8 +230,7 @@ string vectorToString() {
 int main() {
 
 	string instruction="";
-	string result="";
-
+	ConfirmValidation confirmVal;
 	getInitText();
 
 	while (1) {
@@ -174,7 +239,8 @@ int main() {
 		showInitTextView(page);
 		make_bottom(result);
 		result = "";
-		cin >> instruction;
+		//cin >> instruction;
+		getline(cin, instruction);
 		if (instruction.length() < 2) {
 			if (!instruction.compare("n") || !instruction.compare("p") || !instruction.compare("t")) {
 				if (instruction.at(0) == 'n') {
@@ -195,17 +261,12 @@ int main() {
 					saveAndExit();
 				}
 			}
-			else if (!instruction.compare("i") || !instruction.compare("d") || !instruction.compare("s") || !instruction.compare("c")) {
-				result = "you are wrong. please enter completely (your instruction was => "+instruction+") recomended: "+instruction+"(valid parameters)";
-				// recomended 띄우기
-			}
-			else {
-				result = "("+instruction+")"+" is not valid instruction";
-			}
+			
+			
 		}
 		else {
 			if (instruction.at(0) == 'i') {
-				if (isValid(instruction)) {
+				if (confirmVal.isValid(instruction)) {
 					string substr = instruction.substr(2, instruction.length()-3);
 					vector<string> tokens;
 					string buf;
@@ -215,49 +276,52 @@ int main() {
 						tokens.push_back(buf);
 					}
 
-					int row = 0;
-					stringstream ssInt(tokens[0]);
-					ssInt >> row;
+					if (confirmVal.InsertInstructionIsWrong(tokens)) {
+						int row = 0;
+						stringstream ssInt(tokens[0]);
+						ssInt >> row;
+						int col = 0;
+						stringstream ssInt2(tokens[1]);
+						ssInt2 >> col;
+						if (confirmVal.isOver(row,col)) {
 
-					int col = 0;
-					stringstream ssInt2(tokens[1]);
-					ssInt2 >> col;
+							string InsertingStr;
+							InsertingStr = tokens[2];
 
-					string InsertingStr;
-					InsertingStr = tokens[2];
-
-					string getstrFromSavingSentence = savingSentences[row-1];
+							string getstrFromSavingSentence = savingSentences[row-1];
 	
-					int countForInsert = 0;
-					string newSentence;
-					bool inserted = false;
+							int countForInsert = 0;
+							string newSentence;
+							bool inserted = false;
 
-					int getstrFromSavingSentenceLength = getstrFromSavingSentence.length();
+							int getstrFromSavingSentenceLength = getstrFromSavingSentence.length();
 
-					for (int i = 0; i < getstrFromSavingSentenceLength; i++) {
-						if ((getstrFromSavingSentence[i] == ' ')&&!inserted)countForInsert++;
-						if ((countForInsert == col) && !inserted) {
-							if (col == 0) {
-								newSentence += InsertingStr + " ";
-								i--;
+							for (int i = 0; i < getstrFromSavingSentenceLength; i++) {
+								if ((getstrFromSavingSentence[i] == ' ')&&!inserted)countForInsert++;
+								if ((countForInsert == col) && !inserted) {
+									if (col == 0) {
+										newSentence += InsertingStr + " ";
+										i--;
+									}
+									else
+									{
+										newSentence += " "+InsertingStr+" ";
+									}
+									inserted = true;
+									continue;
+								}
+						
+								newSentence += getstrFromSavingSentence[i];
+						
 							}
-							else
-							{
-								newSentence += " "+InsertingStr+" ";
-							}
-							inserted = true;
-							continue;
+							savingSentences[row - 1] = newSentence;
+							savingSentencesWithString = vectorToString();
 						}
-						
-						newSentence += getstrFromSavingSentence[i];
-						
 					}
-					savingSentences[row - 1] = newSentence;
-					savingSentencesWithString = vectorToString();
 				}
 			}
 			else if (instruction.at(0) == 'd') {
-				if (isValid(instruction)) {
+				if (confirmVal.isValid(instruction)) {
 					string substr = instruction.substr(2, instruction.length() - 3);
 					vector<string> tokens;
 					string buf;
@@ -301,7 +365,7 @@ int main() {
 				}
 			}
 			else if (instruction.at(0) == 's') {
-				if (isValid(instruction)) {
+				if (confirmVal.isValid(instruction)) {
 					string substr = instruction.substr(2, instruction.length() - 3);
 					int deleteLocationX = -1;
 					int deleteLocationY = -1;
@@ -340,7 +404,7 @@ int main() {
 				}
 			}
 			else if (instruction.at(0) == 'c') {
-				if (isValid(instruction)) {
+				if (confirmVal.isValid(instruction)) {
 					string substr = instruction.substr(2, instruction.length() - 3);
 					vector<string> tokens;
 					string buf;
@@ -367,6 +431,9 @@ int main() {
 
 					savingSentencesWithString = vectorToString();
 				}
+			}
+			else {
+				result = "(" + instruction + ")" + " is not valid instruction";
 			}
 
 		}
